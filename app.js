0820f6b22735017fad8f7c28bccc9fd7f615c7df
fumiya <fu38.terada@gmail.com>
2019-07-30 15:45:09 +0900

    first commit
    7/29 2arrayをjsにて作成

---
+++ app.js
@@ -0,0 +1,8 @@
+let fruits_colors = [
+  ['apple','りんご','orange','みかん'],
+  ['pink','ピンク','red','赤']
+];
+
+console.log(fruits_colors);
+console.log(fruits_colors[0]);
+console.log(fruits_colors[1]);
