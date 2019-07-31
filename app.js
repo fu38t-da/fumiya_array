80851859725e9110ea199de3c1185a7df0a6bc8b
fumiya <fu38.terada@gmail.com>
2019-07-31 10:57:49 +0900

    3arrayを追加
    3次元配列を追加しました。

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//2array
+
 let fruits_colors = [
   ['apple','りんご','orange','みかん'],
   ['pink','ピンク','red','赤']
@@ -6,3 +8,19 @@ let fruits_colors = [
 console.log(fruits_colors);
 console.log(fruits_colors[0]);
 console.log(fruits_colors[1]);
+
+//3array
+
+let array = [
+  {
+      HP: 250,
+      MP: 30,
+      attack: [
+        '攻撃',
+        '必殺技',
+        '逃げる'
+    ],
+  },
+];
+
+console.log(array);
